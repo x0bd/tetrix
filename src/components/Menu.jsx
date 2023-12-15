c956f249934e14a01ba574638018740c15d772f4
Tinodaishe Tembo <synthmancer@gmail.com>
2023-12-15 14:14:33 +0200

    major refactor

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import '../css/menu.css'
+
+const Menu = ({ onClick }) => (
+  <div className="Menu">
+    <button className="Button" onClick={onClick}>
+      Play Tetris
+    </button>
+  </div>
+);
+
+export default Menu;
